083ee325e6d1afb1369873bc3f43ce011d718c58
Mementhe <clemseb.bauer@gmail.com>
2022-07-29 15:24:21 +0200

    vector upate

--- source/module1.py
+++ source/module1.py
@@ -9,30 +9,31 @@ def non_rigid_pcd(input1, input2):
     target = o3.io.read_point_cloud(input2)
     o3.visualization.draw_geometries([target, source])
 
-    target = target.voxel_down_sample(voxel_size=0.5)
-    source = source.voxel_down_sample(voxel_size=0.5)
+    target = target.voxel_down_sample(voxel_size=2.5)
+    source = source.voxel_down_sample(voxel_size=2.5)
     # compute cpd registration
     tf_param, _, _ = cpd.registration_cpd(source, target)
     result = copy.deepcopy(source)
     result.points = tf_param.transform(result.points)
 
     pcd = open("../data/in/XYZ.txt")
-    pcdtxt = pcd.readlines
-    for line in pcdtxt:
-            if line == 0:
-                station = line.split(",")
-                scanX = float(station[0])
-                scanY = float(station[1])
-                scanZ = float(station[2])
-                target.translate((scanX, scanY, scanZ), relative = True)
-                source.translate((scanX, scanY, scanZ), relative = True)
-            if line == 1:
-                station = line.split(",")
-                scanX = -float(station[0])
-                scanY = -float(station[1])
-                scanZ = -float(station[2])
-                target.translate((scanX, scanY, scanZ), relative = True)
-                source.translate((scanX, scanY, scanZ), relative = True)
+    pcd = pcd.readlines()
+    for line in pcd:
+        station = []
+        if line == 0:
+            station = line.split(",")
+            scanX = float(station[0])
+            scanY = float(station[1])
+            scanZ = float(station[2])
+            target.translate((scanX, scanY, scanZ), relative = True)
+            source.translate((scanX, scanY, scanZ), relative = True)
+        if line == 1:
+            station = line.split(",")
+            scanX = -float(station[0])
+            scanY = -float(station[1])
+            scanZ = -float(station[2])
+            target.translate((scanX, scanY, scanZ), relative = True)
+            source.translate((scanX, scanY, scanZ), relative = True)
     
 
     # draw result
